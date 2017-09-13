d917e2e21b067cccd1da85cb305f003e65326106
Isamu Hiro <isamuhirahata@gmail.com>
2017-09-13 05:05:28 -0300

    (feat)lesson 1 - lesson 1 setup express, routes and databases

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var passport = require('passport');
-var mongoose = require('mongoose');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport-jwt');
+const mongoose = require('mongoose');
+const config = require('./config/database');
 
-var app = express();
+/*Database connection*/
+mongoose.connect(config.database);
+
+mongoose.connection.on('connected',()=>{
+  console.log('Connected to db' + config.database);
+});
+
+mongoose.connection.on('erorr',(err)=>{
+  console.log('database error: '+err);
+});
+/*End Database connection*/
+
+const app = express();
+
+const users = require('./routes/users');
+
+/*CORS middleware*/
+app.use(cors());
+
+/*wefew*/
+app.use(express.static(path.join(__dirname,'public')));
+
+/*bodyParser*/
+app.use(bodyParser.json());
+
+/*Index route*/
+app.get('/',(req,res) => {
+  res.send('Invalid endpoit');
+});
+
+app.use('/users',users);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.listen(8888,() =>{
   console.log('Server starting on port 8888');
