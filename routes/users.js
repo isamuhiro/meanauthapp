9d9fca63b2bb58fba796d3159336a50b16c35713
Isamu Hiro <isamuhirahata@gmail.com>
2017-09-13 05:45:03 -0300

    (feat) persisting database

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport-jwt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-router.get('/register', (req,res,next) => {
-    res.send('Register');
+router.post('/register', (req,res,next) => {
+    let newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    User.addUser(newUser,(err, user)=>{
+      if(err){
+        res.json({sucess:false,msg:'Failed to register user'+err});
+      }else{
+        res.json({sucess:true,msg:'User registred'});
+      }
+    })
 });
 
-router.get('/authenticate', (req,res,next) => {
+router.post('/authenticate', (req,res,next) => {
     res.send('Authenticate');
 });
 
 router.get('/profile', (req,res,next) => {
     res.send('Profile');
-});
-
-router.get('/validate', (req,res,next) => {
-    res.send('Validate');
 });
 
 module.exports = router;
