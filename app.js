c09fd5959c9c870da342df14b397c68d4b5d723c
Isamu Hiro <isamuhirahata@gmail.com>
2017-09-13 04:41:11 -0300

    (fix)README.md - just letting pretty;

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var passport = require('passport');
+var mongoose = require('mongoose');
+
+var app = express();
+
+app.listen(8888,() =>{
+  console.log('Server starting on port 8888');
+})
